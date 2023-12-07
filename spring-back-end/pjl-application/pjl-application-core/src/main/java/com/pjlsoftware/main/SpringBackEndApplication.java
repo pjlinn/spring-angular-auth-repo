c58eb701193de0dc22f95c8b30180c08b25ade19
pjl <p.linnehan@gmail.com>
2023-12-06 22:10:41 -0500

    checkpoint commit
    health check not working

---
+++ spring-back-end/pjl-application/pjl-application-core/src/main/java/com/pjlsoftware/main/SpringBackEndApplication.java
@@ -0,0 +1,28 @@
+package com.pjlsoftware.main;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@ImportResource({"classpath:applicationContext.xml"})
+@EntityScan(basePackageClasses = {
+        SpringBackEndApplication.class,
+        Jsr310JpaConverters.class
+})
+public class SpringBackEndApplication {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(SpringBackEndApplication.class, args);
+    }
+}
